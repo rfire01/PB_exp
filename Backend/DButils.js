d7bd559c876781edc05d1ef6d54bf39783f5d1e8
boazAvami <86920027+boazAvami@users.noreply.github.com>
2021-11-30 22:51:26 +0200

    fix: connection ending

--- Backend/DButils.js
+++ Backend/DButils.js
@@ -17,18 +17,18 @@ async function dbQuery(databaseQuery) {
   return new Promise(async (resolve, reject)  => {
 
   //local
-  let conProm = getLocalDbConnection();
-  await conProm.connect(function(err) {
-    if (err) reject(e);
-    else console.log("local Db");
-  });
+  // let conProm = getLocalDbConnection();
+  // await conProm.connect(function(err) {
+  //   if (err) reject(e);
+  //   else console.log("local Db");
+  // });
 
   //production
-  // let conProm = await getProductionDbConnectionConnectedPromise().catch( error => {
-  //   fs.createWriteStream(path.join("./", 'error.log'), {flags: 'a'});
-  //   fs.appendFileSync("./error.log",new Date(parseInt(new Date().getTime())).toString()+ ' - SQL ERROR: ' + error.sqlMessage + '\n');
-  //   reject(e.sqlMessage);
-  // });
+  let conProm = await getProductionDbConnectionConnectedPromise().catch( error => {
+    fs.createWriteStream(path.join("./", 'error.log'), {flags: 'a'});
+    fs.appendFileSync("./error.log",new Date(parseInt(new Date().getTime())).toString()+ ' - SQL ERROR: ' + error.sqlMessage + '\n');
+    reject(e.sqlMessage);
+  });
   
   conProm.query(databaseQuery, function (error, result) {
     conProm.end();
@@ -104,7 +104,6 @@ async function mul_dbQuery(queries) {
       //     database: 'expKobedev',
       //   }
       // );
-
       // conProm.connect(function(err) {
       //   if (err)  console.log(err);
       //   else console.log("Connected!");
